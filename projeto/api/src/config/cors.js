981a684cb77e7ef1d7deabbbd3cff56941be953c
Gfragata <gfragata97@gmail.com>
2020-06-09 22:48:53 -0300

    Subida da tela cursos + ajustes na home e no menu

--- projeto/api/src/config/cors.js
+++ projeto/api/src/config/cors.js
@@ -1,6 +1,14 @@
+// exporta uma função que recebe request, response e next
 module.exports = (_, response, next) => {
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    response.header("Access-Control-Allow-Headers", "Origin,X - Requested - With, Content - Type, Accept");
+    // adiciona nos headers do response que todas as origens estão permitidas
+    response.header("Access-Control-Allow-Origin", "*"); 
+
+    // adiciona nos headers do response que somente estes metodos HTTP estão permitidos
+    response.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE"); 
+
+    // adiciona nos headers do response que somente estes headers estão permitidos
+    response.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+    
+    // chama o next para dar continuidade na requisição
     next();
 }
